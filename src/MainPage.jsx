fb98cd41b366629921b537708b81b697deba1809
nadirflow <122340945+nadirflow@users.noreply.github.com>
2023-02-10 15:50:21 +0500

    Update MainPage.jsx

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -1,30 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Toolbar, Page, Button} from 'react-onsenui';
+import React,  { useState }  from 'react';
+import { Tabbar, Page, Toolbar, Tab, Button, Input } from 'react-onsenui';
 
-import SecondPage from './SecondPage'
 
-export default class MainPage extends React.Component {
-  pushPage() {
-    this.props.navigator.pushPage({component: SecondPage});
+
+export default function MainPage() {
+ 
+
+  
+
+  const renderTabs = () => {
+    return [
+      {
+        content: <FirstPage />,
+        tab: <Tab label="Facebook" icon="md-facebook" />
+      },
+      {
+        content: <SecondPage />,
+        tab: <Tab label="Instagram" icon="md-instgram" />
+      }
+    ];
+  };
+
+  return (
+    <Tabbar renderTabs={renderTabs} />
+  );
+}
+
+function FirstPage() {
+  const [value, setValue] = useState('');
+  const [error, setError] = useState(null);
+const handleSubmit = () => {
+    if (!value.match(/^https?:\/\/\S+$/)) {
+      setError('Invalid URL');
+    } else {
+      setError(null);
+      // Perform further action
+    }
   }
+  return (
+    <Page style={{padding:'10'}}>
+     
+      <h2 style={{ fontWeight: '700', textAlign: 'center' }}>Facebook</h2>
+      <p style={{ fontWeight: '400', textAlign: 'center' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultricies lectus lorem.</p>
+      <Input
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
+        type="url"
+        placeholder="Enter your name"
+      />
+      <Button onClick={handleSubmit}>Submit</Button>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+    </Page>
+  );
+}
 
-  renderToolbar() {
-    return (
-      <Toolbar style={{backgroundColor: '#0054AC'}}>
-        <div className="center" style={{color:'#fff'}}>Social Media Downloader</div>
-      </Toolbar>
-    );
-  }
-
-  render() {
-    return (
-      <Page renderToolbar={this.renderToolbar} style={{backgroundColor: '#000'}}>
-        <p>Hello</p>
-        <p style={{textAlign: 'center'}}>
-          <Button onClick={this.pushPage.bind(this)}>Push page</Button>
-        </p>
-      </Page>
-    );
-  }
+function SecondPage() {
+  return (
+    <Page>
+      
+      <h2 style={{ fontWeight: '700', textAlign: 'center' }}>Instagram</h2>
+      <p style={{ fontWeight: '400', textAlign: 'center' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultricies lectus lorem.</p>
+    </Page>
+  );
 }
